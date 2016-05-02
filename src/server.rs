5cdb3404aae6d38645539cf2a73e2c551d114cae
Matt Ball <matt.ball.2@gmail.com>
2016-05-02 09:39:19 -0700

    adds a simple server

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,14 @@
-fn main() {
+#[macro_use] extern crate nickel;
 
+use nickel::Nickel;
+
+
+fn main() {
+	let mut server = Nickel::new();
+	server.utilize(router! {
+		get "**" => |_req, _res| {
+			"hi!"
+		}
+	});
+	server.listen("127.0.0.1:6767");
 }
